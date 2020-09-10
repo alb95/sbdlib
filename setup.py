3049105506da4de45eced83a21523fdb249b7538
alb95 <57409084+alb95@users.noreply.github.com>
2020-09-10 13:57:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='sbdlib', # SoBigData Library
       description='SoBigData library',
       long_description=long_description,
       long_description_content_type="text/markdown",
-      url="https://github.com/pypa/sampleproject",
+      url='https://github.com/alb95/NetworkProject',
       packages=setuptools.find_packages(),
       classifiers=[                                       #???????????
           "Programming Language :: Python :: 3",          #???????????
